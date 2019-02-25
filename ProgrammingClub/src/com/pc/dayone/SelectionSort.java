046455a01c7b363cd7a24a4ea982eb15f24d35d2
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2019-02-25 11:11:28 -0500

    Fixed problem

--- ProgrammingClub/src/com/pc/dayone/SelectionSort.java
+++ ProgrammingClub/src/com/pc/dayone/SelectionSort.java
@@ -1,5 +1,9 @@
 package com.pc.dayone;
 
 public class SelectionSort {
-
+	public static void main(String args[]) {
+		for(int i = 0; i < 17; i++) {
+			System.out.println("Hahahahaha");
+		}
+	}
 }
